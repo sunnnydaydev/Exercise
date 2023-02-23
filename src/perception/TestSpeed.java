238479c970a1be282bde134d4a3a6a2cc417199a
SunnyDay <biao.zhang@zennioptical.com>
2023-02-23 10:43:33 +0800

    RomanToInt 感悟

---
+++ src/perception/TestSpeed.java
@@ -0,0 +1,47 @@
+package perception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestSpeed {
+    public static void main(String[] args) {
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        // 感悟：简单的映射关系。方法比使用map速度更快。更加节省内存。
+    }
+
+    private int getNumberByNormalChar(char c) {
+        int num = 0;
+        switch (c) {
+            case 'I':
+                num = 1;
+                break;
+            case 'V':
+                num = 5;
+                break;
+            case 'X':
+                num = 10;
+                break;
+            case 'L':
+                num = 50;
+                break;
+            case 'C':
+                num = 100;
+                break;
+            case 'D':
+                num = 500;
+                break;
+            case 'M':
+                num = 1000;
+                break;
+        }
+        return num;
+    }
+}
